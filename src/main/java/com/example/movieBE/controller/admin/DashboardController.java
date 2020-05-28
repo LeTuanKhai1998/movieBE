0ff24157c68730d93880862b00ff1e348c7e0a67
KhaiLT <KhaiLT@splus-software.com.vn>
2020-05-28 20:47:13 +0700

    add auth login

--- src/main/java/com/example/movieBE/controller/admin/DashboardController.java
+++ src/main/java/com/example/movieBE/controller/admin/DashboardController.java
@@ -1,13 +1,22 @@
 package com.example.movieBE.controller.admin;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = {"*"})
 @RestController
 @RequestMapping("/api/admin")
 public class DashboardController {
+    /**
+     *
+     */
+    public static Logger logger = LoggerFactory.getLogger(DashboardController.class);
 
-
+    @GetMapping(path = "/Secured")
+    public @ResponseBody
+    String getSecured() {
+        logger.info("GET successfully called on /secured resource");
+        return "SECURED_TEXT";
+    }
 }
